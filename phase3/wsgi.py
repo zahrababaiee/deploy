3d855a9fc672baa7ec38fae0969125679d1ee204
zahrababaiee <z.babaiee@gmail.com>
2017-07-22 17:45:20 +0430

    my final commit

--- phase3/wsgi.py
+++ phase3/wsgi.py
@@ -4,7 +4,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "phase3.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
